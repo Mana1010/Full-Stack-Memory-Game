206124fcc2608c3d195201d2b26c331dfa237f43
Mana1010 <tristanvicclarito2003@gmail.com>
2024-04-11 23:10:32 +0800

    Changing something

--- client/src/app/(auth)/auth/signup/page.tsx
+++ client/src/app/(auth)/auth/signup/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import icon from "../../../../components/images/logo.png";
 import { FaStar } from "react-icons/fa6";
 import { useForm } from "react-hook-form";
 import { string, z } from "zod";
+import Link from "next/link";
+import memoryBg from "../../../../components/images/icon.png";
 // const obj = z.object({
 //   username: string(),
 //   password: string().min(7, "Password must be at least 7 characters long."),
@@ -16,6 +18,16 @@ import { string, z } from "zod";
 // type Form = z.infer<obj>;
 function Signup() {
   const { formState, register } = useForm();
+  const [hasChanged, setHasChanged] = useState(true);
+  useEffect(() => {
+    function load(e: BeforeUnloadEvent) {
+      e.preventDefault();
+    }
+    if (hasChanged) {
+      window.addEventListener("beforeunload", load);
+      return () => window.removeEventListener("beforeunload", load);
+    }
+  }, []);
   return (
     <main
       id="bg"
@@ -28,7 +40,7 @@ function Signup() {
         <form
           autoComplete="false"
           id="form"
-          className="px-4 py-3 w-full sm:w-[350px] h-[400px] backdrop-blur-sm rounded-sm mx-auto relative"
+          className="px-4 py-3.5 w-full sm:w-[400px] h-[450px] backdrop-blur-sm rounded-sm mx-auto relative"
         >
           {/* TOP AND LEFT */}
           <div
@@ -68,7 +80,7 @@ function Signup() {
           ></div>
           <header className="w-full flex justify-between items-center">
             <h1 className="text-white text-xl">REGISTER</h1>
-            <span className="text-[#FFE30A] text-2xl">
+            <span className="text-[#FFE30A] text-3xl">
               <FaStar />
             </span>
           </header>
@@ -85,7 +97,7 @@ function Signup() {
                 {...register("username")}
                 name="username"
                 id="username"
-                className="w-full p-2 space-x-2 rounded-sm bg-primary outline-none text-white"
+                className="w-full p-2.5 space-x-2 rounded-sm bg-primary text-white outline-[#EBD30C] border-none outline-dashed outline-1"
               />
             </div>
             <div className="space-y-1 flex flex-col">
@@ -95,7 +107,7 @@ function Signup() {
               >
                 PASSWORD
               </label>
-              <div className="w-full p-2 space-x-2 rounded-sm bg-primary">
+              <div className="w-full p-2.5 space-x-2 rounded-sm bg-primary outline-[#EBD30C] outline-dashed outline-1">
                 <input
                   type="password"
                   {...register("password")}
@@ -112,7 +124,7 @@ function Signup() {
               >
                 CONFIRM PASSWORD
               </label>
-              <div className="w-full p-2 space-x-2 rounded-sm bg-primary">
+              <div className="w-full p-2.5 space-x-2 rounded-sm bg-primary outline-[#EBD30C] outline-dashed outline-1">
                 <input
                   type="password"
                   {...register("confirm-password")}
@@ -121,6 +133,24 @@ function Signup() {
                   className="bg-transparent text-white outline-none w-[90%]"
                 />
               </div>
+            </div>
+            <div className="pt-2 w-full flex justify-center flex-col items-center space-y-2">
+              <button
+                style={{ boxShadow: "0 0 8px #ffe30a" }}
+                id="button-submit"
+                type="submit"
+                className="w-[200px] py-2.5 bg-[#EBD30C] text-primary rounded-md transition-all duration-200 ease-in"
+              >
+                SUBMIT
+              </button>
+              <p className="text-white">
+                Already have an account?{" "}
+                <span className="text-[#EBD30C] underline-offset-2 underline">
+                  <Link href={"/auth/login"} className="">
+                    login here
+                  </Link>
+                </span>
+              </p>
             </div>
           </div>
         </form>
