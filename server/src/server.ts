e70a3cbc1a49d00480bb01118c522a573c90eb8a
Mana1010 <tristanvicclarito2003@gmail.com>
2024-08-08 19:23:10 +0800

    Again:(((

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { router as authRouter } from "./routes/auth.route";
@@ -17,7 +16,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
