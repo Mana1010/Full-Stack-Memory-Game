c7f1db00b24735107eb5b690f663ccb9ec16a3b1
Mana1010 <tristanvicclarito2003@gmail.com>
2024-08-08 22:32:19 +0800

    redeploy again

--- client/src/utils/baseUrl.ts
+++ client/src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://full-stack-memory-game.onrender.com";
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://full-stack-memory-game.onrender.com";
+// export const baseUrl = "http://localhost:8080";
