50e8eaf3b1d16bedc078edb740ac3bedffcdfff5
Mana1010 <tristanvicclarito2003@gmail.com>
2024-08-08 10:23:55 +0800

    Again

--- server/global.d.ts
+++ server/global.d.ts
@@ -2,14 +2,14 @@ import { Types } from "mongoose";
 
 interface User {
   username?: string;
-  _id?: Types.ObjectId;
+  _id: Types.ObjectId;
   isOldUser?: boolean;
 }
 
 declare global {
   namespace Express {
     interface Request {
-      user: User | null;
+      user?: User | null;
     }
   }
 }
